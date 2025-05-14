b6b11c6dda2d334208f6bd2ac8e275b4f772b345
Mitch Stephan <mitch.stephan@compass.com>
2025-05-14 13:36:20 -0700

    add pipx and bin to path

--- zshrc/exports.zsh
+++ zshrc/exports.zsh
@@ -1,5 +1,5 @@
 export EDITOR="vim"
 export NVM_DIR="$HOME/.nvm"
-export PATH="/usr/local/sbin:$PATH"
+export PATH="/usr/local/sbin:~/.local/bin:$PATH"
 export SSH_KEY_PATH="~/.ssh/rsa_id"
 export BREWHOME=$(brew --prefix)
